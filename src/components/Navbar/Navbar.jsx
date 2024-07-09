d277fca6c7d053b41d4729b7d784602d3aa80a3c
Andrew Vergun <andrewvergun@gmail.com>
2024-07-09 21:25:05 +0200

    Final commit

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,32 +1,30 @@
-import React from 'react'
-import logo from '../../assets/logo.svg'
-import './Navbar.css'
-import { Link } from 'react-router-dom'
-
+import React from "react";
+import logo from "../../assets/logo.svg";
+import "./Navbar.css";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <>
-        <nav className='navbar'>
-            <div className="logo">
-                <Link to="/" className='logo-link'>
-                    <img className="logo-icon" src={logo} alt="" />
-                    <h1 className='logo-text'>BookVault</h1>
-                </Link>
-            </div>
-            <ul>
-                <Link to="/"><li>Home</li></Link>
-                <Link to="/about"><li>About</li></Link>
-            </ul>
-            <div className="free-space">
-                
-            </div>
-                
-            
-            
-        </nav>
+      <nav className="navbar">
+        <div className="logo">
+          <Link to="/" className="logo-link">
+            <img className="logo-icon" src={logo} alt="" />
+            <h1 className="logo-text">BookVault</h1>
+          </Link>
+        </div>
+        <ul>
+          <Link to="/">
+            <li>Home</li>
+          </Link>
+          <Link to="/about">
+            <li>About</li>
+          </Link>
+        </ul>
+        <div className="free-space"></div>
+      </nav>
     </>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
